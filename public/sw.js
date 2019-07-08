39383e35228821f7638d740ac8bea9350967c85d
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2019-07-08 22:02:39 +0200

    service-workers-caching

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,76 @@
+var CACHE_STATIC_NAME = 'static-4';
+var CACHE_DYNAMIC_NAME = 'dynamic-4';
+
 //give me access the sw background proceess
-self.addEventListener('install',function (event) {
-    console.log('[sw] Installing service worker ...' ,event);
+self.addEventListener('install', function (event) {
+    //console.log('[sw] Installing service worker ...' ,event);
+    event.waitUntil(
+        caches.open(CACHE_STATIC_NAME)
+            .then(function (cache) {
+                console.log('[Service Worker] Precaching App Shell ....');
+                // cache.add('/src/js/app.js');
+                // Cache Multiple Files with addAll
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/src/js/app.js',
+                    '/src/js/feed.js',
+                    '/src/js/promise.js',
+                    '/src/js/fetch.js',
+                    '/src/js/material.min.js',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/css/help.css',
+                    '/src/images/main-image.jpg',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+                ]);
+            })
+    )
 });
 
-// close tab and then reload 
-self.addEventListener('activate',function (event) {
-    console.log('[sw] Activiating service worker ...' ,event);
+// close tab and then reload  new website again 
+self.addEventListener('activate', function (event) {
+    console.log('[sw] Activiating service worker ...', event);
+    // Different Cache Versions Cleanup
+    caches.keys()
+        .then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                    console.log('[sw] Removing Old Cache ...', key);
+                    return caches.delete(key);
+                }
+            }))
+        })
     return self.clients.claim();
 })
 
 
-self.addEventListener('fetch',function (event) {
-    console.log('[sw] Fetching service worker ...' ,event);
-    // override the response for evey fetch
-    // return event.resposeWith(null);
+self.addEventListener('fetch', function (event) {
+    console.log('[sw] Fetching service worker ...', event);
+    // Retrieving Items from the Cache...
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (response) {
+                    return response;
+                } else {
+                    // send the original request
+                    return fetch(event.request)
+                        .then(function (res) {
+                            // create a dynammic cahce
+                            return caches.open(CACHE_DYNAMIC_NAME)
+                                .then(function (cache) {
+                                    cache.put(event.request.url, res.clone());
+                                    return res;
+                                })
+                        })
+                        //Handling Errors
+                        .catch(function (err) {
+                            console.log(err);
+                        });
+                }
+            })
+    )
 })
